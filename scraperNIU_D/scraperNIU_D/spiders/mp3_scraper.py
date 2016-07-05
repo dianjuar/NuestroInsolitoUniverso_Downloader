02d511a1719c7b2765cfcdf6bb85fbb7222b53e0
dianjuar <dianjuar@gmail.com>
2016-07-04 23:07:06 -0400

    Got the urls of the post of the 1st page

--- scraperNIU_D/scraperNIU_D/spiders/mp3_scraper.py
+++ scraperNIU_D/scraperNIU_D/spiders/mp3_scraper.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy			import Selector
+from scrapy.http	import Request
 
 
 class Mp3ScraperSpider(scrapy.Spider):
@@ -9,6 +11,25 @@ class Mp3ScraperSpider(scrapy.Spider):
 		'http://ondalasuperestacion.com/category/categorias-principal/micros-categorias-principal/insolito-universo/',
 	)
 
-	def parse(self, response):
-		# hxs = Selector(response)
+	def __init__(self):
+		self._1stpage = True
+		self.postList = list()
 		pass
+
+	def parse(self, response):
+		hxs = Selector(response)
+		base = hxs.xpath('//section[@id="ContinentWrapper"]')
+
+		# if is the 1st time of the scraper, scrap the banner
+		if self._1stpage is True:
+			
+			self._1stpage = False
+			url = base.xpath('.//section[@class="col-xs-12"]//a/@href')
+			# print("********************************")
+			self.postList.append( url.extract() )
+
+		urls = base.xpath('.//section[@class="col-xs-12 col-sm-12 col-md-8 col-lg-8"]//a/@href')
+
+		self.postList += urls.extract()
+		print( len(self.postList) )
+		
